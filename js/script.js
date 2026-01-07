64ae9be906f06632b537513f4c4e1f13c271b1b4
Moreno <jesusmorenomm1@gmail.com>
2026-01-06 20:56:07 -0600

    function to add html tag p with appendChild to show the result of attacks between player and enemy, function to generate a random attack enemy

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+let attackPlayer = '';
+let attackEnemy = '';
+
 function startGame() {
   let buttonPetPlayer = document.getElementById('button-pet');
   buttonPetPlayer.addEventListener('click', selectionPetPlayer);
+
+  let buttonFire = document.getElementById('button__fire');
+  buttonFire.addEventListener('click', attackFire);
+
+  let buttonWater = document.getElementById('button__water');
+  buttonWater.addEventListener('click', attackWater);
+
+  let buttonGround = document.getElementById('button__ground');
+  buttonGround.addEventListener('click', attackGround);
 }
 
+function attackFire() {
+    attackPlayer = 'Fire';
+    spanAttackEnemy();
+}
+function attackWater() {
+    attackPlayer = 'Water';
+    spanAttackEnemy();
+}
+function attackGround() {
+    attackPlayer = 'Ground';
+    spanAttackEnemy();
+}
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -34,15 +58,34 @@ function selectionPetPlayer() {
 function selectionPetEnemy() {
   let petEnemy = document.getElementById('pet__enemy');
   let numberEnemy = random(1, 3);
-  if(numberEnemy == 1){
-    petEnemy.innerHTML = 'Chamaleon'
-  } else if(numberEnemy == 2){
-    petEnemy.innerHTML = 'Turtle'
-  } else if(numberEnemy == 3){
-    petEnemy.innerHTML = 'Basur'
+  if (numberEnemy == 1) {
+    petEnemy.innerHTML = 'Chamaleon';
+  } else if (numberEnemy == 2) {
+    petEnemy.innerHTML = 'Turtle';
+  } else if (numberEnemy == 3) {
+    petEnemy.innerHTML = 'Basur';
   }
 
   return petEnemy;
+}
+
+function spanAttackEnemy() {
+  let numberEnemy = random(1, 3);
+  if (numberEnemy == 1) {
+    attackEnemy = 'Fire';
+  } else if (numberEnemy == 2) {
+    attackEnemy = 'Water';
+  } else if (numberEnemy == 3) {
+    attackEnemy = 'Ground';
+  }
+  createMessage();
+}
+
+function createMessage() {
+  let paragraph = document.createElement('p');
+  let sectionMessages = document.getElementById('messages');
+  paragraph.innerHTML = 'Your pet attacks with ' + attackPlayer + ', pet of enemy attacks with ' + attackEnemy;
+  sectionMessages.appendChild(paragraph);
 }
 
 window.addEventListener('load', startGame);
