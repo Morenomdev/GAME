2a6e3bd0c8bd2318849bb5f22dff6b4394ed3403
Moreno <jesusmorenomm1@gmail.com>
2026-01-06 18:25:42 -0600

    function selectionPetEnemy added with random number, and replace in html with .innerHTML

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 function startGame() {
   let buttonPetPlayer = document.getElementById('button-pet');
-  buttonPetPlayer.addEventListener('click', selectionPetPLayer);
+  buttonPetPlayer.addEventListener('click', selectionPetPlayer);
 }
 
-function selectionPetPLayer() {
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function selectionPetPlayer() {
   let petSelected = '';
   let inputChamaleon = document.getElementById('chamaleon');
   let inputTurtle = document.getElementById('turtle');
   let inputBasur = document.getElementById('basur');
+  let spanPetPlayer = document.getElementById('pet__player');
+  //   let petEnemy =
+
   if (inputChamaleon.checked) {
     petSelected = 'chamaleon';
+    spanPetPlayer.innerHTML = 'Chamaleon';
   } else if (inputTurtle.checked) {
     petSelected = 'turtle';
+    spanPetPlayer.innerHTML = 'Turtle';
   } else if (inputBasur.checked) {
     petSelected = 'basur';
+    spanPetPlayer.innerHTML = 'Basur';
   } else {
-    alert('You should select a pet')
+    alert('You should select a pet');
   }
-    document.getElementById('pet-player').innerHTML = petSelected ;
-  return document.getElementById('pet-player').innerHTML ;
+  selectionPetEnemy();
+  return petSelected;
 }
 
+function selectionPetEnemy() {
+  let petEnemy = document.getElementById('pet__enemy');
+  let numberEnemy = random(1, 3);
+  if(numberEnemy == 1){
+    petEnemy.innerHTML = 'Chamaleon'
+  } else if(numberEnemy == 2){
+    petEnemy.innerHTML = 'Turtle'
+  } else if(numberEnemy == 3){
+    petEnemy.innerHTML = 'Basur'
+  }
 
-if( selectionPetPLayer().length > 0 ){
+  return petEnemy;
 }
-
-
 
 window.addEventListener('load', startGame);
