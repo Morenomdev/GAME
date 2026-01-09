60e84e61b69e37105cb7f608dbeb999959ad4291
Moreno <jesusmorenomm1@gmail.com>
2026-01-08 19:03:30 -0600

    convert some variables in JS to const

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-let sectionSelectAttack = document.getElementById('select-attack');
-let sectionReset = document.getElementById('section-reset');
-let divMessage = document.getElementById('messages');
-let divLifes = document.getElementById('card-attack-div');
-let buttonPetPlayer = document.getElementById('button-pet');
-let buttonFire = document.getElementById('button__fire');
-let buttonWater = document.getElementById('button__water');
-let buttonGround = document.getElementById('button__ground');
-let buttonReset = document.getElementById('button-reset');
+// HTML VARIABLES
+const sectionSelectAttack = document.getElementById('select-attack');
+const sectionReset = document.getElementById('section-reset');
+const divMessage = document.getElementById('messages');
+const divLifes = document.getElementById('card-attack-div');
+const buttonPetPlayer = document.getElementById('button-pet');
+const buttonFire = document.getElementById('button__fire');
+const buttonWater = document.getElementById('button__water');
+const buttonGround = document.getElementById('button__ground');
+const buttonReset = document.getElementById('button-reset');
+const inputChamaleon = document.getElementById('chamaleon');
+const inputTurtle = document.getElementById('turtle');
+const inputBasur = document.getElementById('basur');
+const spanPetPlayer = document.getElementById('pet__player');
+const sectionSelectPet = document.getElementById('select-pet');
+const startMessage = document.getElementById('start-message');
+const resultDiv = document.getElementById('result-div');
+const attackPlayerDiv = document.getElementById('attackPlayer-div');
+const attackEnemyDiv = document.getElementById('attackEnemy-div');
+const petEnemy = document.getElementById('pet__enemy');
 
+let petSelected = '';
 let attackPlayer = '';
 let attackEnemy = '';
 let result = '';
@@ -37,6 +49,25 @@ function startGame() {
   buttonReset.addEventListener('click', reset);
 }
 
+function finalMessage(finalMessage) {
+  //   let paragraph = document.createElement('p');
+  let divMessage = document.getElementById('result-div');
+  divMessage.innerHTML = finalMessage;
+
+  buttonPetPlayer.disabled = true;
+
+  buttonFire.disabled = true;
+
+  buttonWater.disabled = true;
+
+  buttonGround.disabled = true;
+
+  sectionReset.style.display = 'flex';
+  //   sectionReset.style.display = 'flex'
+
+  //   sectionReset.style.display = 'none';
+}
+
 function attackFire() {
   attackPlayer = 'FIRE';
   spanAttackEnemy();
@@ -54,11 +85,6 @@ function random(min, max) {
 }
 
 function selectionPetPlayer() {
-  let petSelected = '';
-  let inputChamaleon = document.getElementById('chamaleon');
-  let inputTurtle = document.getElementById('turtle');
-  let inputBasur = document.getElementById('basur');
-  let spanPetPlayer = document.getElementById('pet__player');
   //   let petEnemy =
   if (
     inputChamaleon.checked == false &&
@@ -67,15 +93,11 @@ function selectionPetPlayer() {
   ) {
     alert('You should select a pet');
   } else {
-    let divMessage = document.getElementById('messages');
     divMessage.style.display = 'flex';
-    let sectionSelectAttack = document.getElementById('select-attack');
     sectionSelectAttack.style.display = 'flex';
 
-    let sectionSelectPet = document.getElementById('select-pet');
     sectionSelectPet.style.display = 'none';
 
-    let divLifes = document.getElementById('card-attack-div');
     divLifes.style.display = 'grid';
 
     if (inputChamaleon.checked) {
@@ -96,7 +118,6 @@ function selectionPetPlayer() {
 }
 
 function selectionPetEnemy() {
-  let petEnemy = document.getElementById('pet__enemy');
   let numberEnemy = random(1, 3);
   if (numberEnemy == 1) {
     petEnemy.innerHTML = 'Chamaleon';
@@ -110,7 +131,6 @@ function selectionPetEnemy() {
 }
 
 function spanAttackEnemy() {
-  let startMessage = document.getElementById('start-message');
   startMessage.style.display = 'none';
   let numberEnemy = random(1, 3);
   if (numberEnemy == 1) {
@@ -126,14 +146,10 @@ function spanAttackEnemy() {
 
 function createMessage() {
   //   let paragraph = document.createElement('p');
-  let resultDiv = document.getElementById('result-div');
-  let attackPlayerDiv = document.getElementById('attackPlayer-div');
-  let attackEnemyDiv = document.getElementById('attackEnemy-div');
-
-  let newAttackPlayer = document.createElement('p');
-  let newAttackEnemy = document.createElement('p');
 
   resultDiv.innerHTML = result;
+  let newAttackPlayer = document.createElement('p');
+  let newAttackEnemy = document.createElement('p');
   newAttackPlayer.innerHTML = attackPlayer;
   newAttackEnemy.innerHTML = attackEnemy;
 
@@ -147,30 +163,6 @@ function createMessage() {
   attackPlayerDiv.appendChild(newAttackPlayer);
   attackEnemyDiv.appendChild(newAttackEnemy);
   checkLife();
-}
-
-function finalMessage(finalMessage) {
-  //   let paragraph = document.createElement('p');
-  let divMessage = document.getElementById('result-div');
-  divMessage.innerHTML = finalMessage;
-
-  let buttonPetPlayer = document.getElementById('button-pet');
-  buttonPetPlayer.disabled = true;
-
-  let buttonFire = document.getElementById('button__fire');
-  buttonFire.disabled = true;
-
-  let buttonWater = document.getElementById('button__water');
-  buttonWater.disabled = true;
-
-  let buttonGround = document.getElementById('button__ground');
-  buttonGround.disabled = true;
-
-  let sectionReset = document.getElementById('section-reset');
-  sectionReset.style.display = 'flex';
-  //   sectionReset.style.display = 'flex'
-
-  //   sectionReset.style.display = 'none';
 }
 
 function combatResult() {
